92b91cb7425d20ccf205fb861c2d0d751804028d
二階堂迅 <dreamnjin1030@gmail.com>
2026-01-06 14:44:04 +0000

    blog&paginate

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -11,14 +11,16 @@ class PostController extends Controller
     //use宣言は外部にあるクラスをPostController内にインポートできる。
     //この場合、App\Models内のPostクラスをインポートしている。
 
-    /**
-     * Post一覧を表示する
-     * 
-     * @param Post Postモデル
-     * @return array Postモデルリスト
-     */
-    public function index(Post $post) //インポートしたPostをインスタンス化して$postとして使用。
-    {
-        return $post->get(); //$postの中身を戻り値にする。
-    }
+  /**
+   * Post一覧を表示する
+   * 
+   * @param Post Postモデル
+   * @return array Postモデルリスト
+   */
+  public function index(Post $post) //インポートしたPostをインスタンス化して$postとして使用。
+  {
+    return view('posts.index')->with(['posts' => $post->getPaginateByLimit()]);
+    //blade内で使う変数'posts'と設定。'posts'の中身にgetを使い、インスタンス化した$postを代入。
+    //getPaginateByLimit()はPost.phpで定義したメソッドです。
+  }
 }
